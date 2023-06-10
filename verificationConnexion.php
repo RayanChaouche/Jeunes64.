b95e86a66ac7304ae705b358142845851766ef99
xymunia <63209212+xymunia@users.noreply.github.com>
2023-06-10 20:42:19 +0200

    Update verificationConnexion.php

--- verificationConnexion.php
+++ verificationConnexion.php
@@ -39,7 +39,7 @@
 
 
 
-    $conditions = (isset($db[$_SESSION['username']]) && strcmp($db[$_SESSION['username']][0], $_SESSION['password']));
+    $conditions = (isset($db[$_SESSION['username']]) && strcmp($db[$_SESSION['username']][0], $_SESSION['password'])==0);
     if($conditions) // si l' username et le password correspondent
     {
         $_SESSION['connecte'] = true;
